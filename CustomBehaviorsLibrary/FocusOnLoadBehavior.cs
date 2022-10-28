162f4285ee4a1d1fd820c4269d836d1864974bad
pegaser <sperezgabriel@gmail.com>
2022-10-28 16:01:24 -0500

    28_OCTUBRE_2022

---
+++ CustomBehaviorsLibrary/FocusOnLoadBehavior.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+
+namespace CustomBehaviorsLibrary
+{
+    public class FocusOnLoadBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.Loaded += AssociatedObject_Loaded;
+        }
+
+        private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.AssociatedObject.Focus();
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.Loaded -= AssociatedObject_Loaded;
+        }
+    }
+}
